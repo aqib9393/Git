4c4b1c73b632835a565577c26d319517fff3efd7
aqib9393 <63776916+aqib9393@users.noreply.github.com>
2022-11-25 01:20:06 +0500

    .

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask,request
+import json
+from thresh_Image import local_threah
 
+import base64
 app= Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -9,6 +12,25 @@ def homepage():
 @app.route('/ap', methods=['GET'])
 def ap():
     return "Ap is working"
+
+@app.route('/upload', methods=[ 'POST'])
+def upload():
+    if request.method=="POST":
+        if request.files['image']:
+            imagefile=request.files['image']
+            imagefile.save("hello.jpg")
+            try:
+                local_threah("hello.jpg")
+                with open("final.jpg", "rb") as img_file:
+                    my_string = base64.b64encode(img_file.read())
+                base64_message = my_string.decode('utf-8')
+                data_set={'img': base64_message,'result': True}
+                json_dump= json.dumps(data_set)
+                return json_dump
+            except:
+                data_set={'img': "Please choose a valid image", 'result':False}
+                json_dump= json.dumps(data_set)
+                return json_dump
 
 if __name__=="__main__":
     app.run()
