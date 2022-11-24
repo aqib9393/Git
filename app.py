55e537ce99320cc3ee45836f51c268c361740414
aqib9393 <63776916+aqib9393@users.noreply.github.com>
2022-11-25 01:38:01 +0500

    .

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import *
 from flask import Flask , request, Response
-from thresh_Image import local_threah
 import json,time,socket
 from werkzeug.utils import secure_filename
 import base64
 from flask_cors import CORS
+import cv2
+import imutils
 
 import base64
 app= Flask(__name__)
