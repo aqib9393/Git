75600c401e9634f88ea9c004a6e60ae1b5cad477
aqib9393 <63776916+aqib9393@users.noreply.github.com>
2022-11-25 01:06:11 +0500

    .

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app= Flask(__name__)
 @app.route('/', methods=['GET'])
 def homepage():
     return "Heroku App"
-    
+
+@app.route('/ap', methods=['GET'])
+def ap():
+    return "Ap is working"
+
 if __name__=="__main__":
     app.run()
