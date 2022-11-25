4d38a2554c70dae05b6ce77fd559923bef8241b2
aqib9393 <63776916+aqib9393@users.noreply.github.com>
2022-11-25 16:13:25 +0500

    .

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 import base64
 from thresh_Image import local_threah
 from werkzeug.utils import secure_filename
-#from flask_cors import CORS
+from flask_cors import CORS
 
 
 app= Flask(__name__)
