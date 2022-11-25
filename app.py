befb020276e6d34fa0097debff6f9e0255fe1899
aqib9393 <63776916+aqib9393@users.noreply.github.com>
2022-11-25 15:06:19 +0500

    .

--- app.py
+++ app.py
@@ -22,8 +22,23 @@ def ap():
 
 @app.route('/upload', methods=[ 'POST'])
 def upload():
-    
-    return "hello"
+    if request.method=="POST":
+        if request.files['image']:
+            imagefile=request.files['image']
+            imagefile.save("hello.jpg")
+            try:
+                local_threah("hello.jpg")
+                with open("final.jpg", "rb") as img_file:
+                    my_string = base64.b64encode(img_file.read())
+                base64_message = my_string.decode('utf-8')
+                data_set={'img': base64_message,'result': True}
+                json_dump= json.dumps(data_set)
+                return json_dump
+            except:
+                data_set={'img': "Please choose a valid image", 'result':False}
+                json_dump= json.dumps(data_set)
+                return json_dump
+
     
 
 if __name__=="__main__":
