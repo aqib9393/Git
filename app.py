530b3bbcc03ccd3f609d0f850df0ed49408fb800
aqib9393 <63776916+aqib9393@users.noreply.github.com>
2022-11-25 14:59:14 +0500

    .

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask , request, Response
 import json
 import base64
 from werkzeug.utils import secure_filename
-from flask_cors import CORS
+#from flask_cors import CORS
 import cv2
 import imutils
+import numpy as np
 
 
 app= Flask(__name__)
