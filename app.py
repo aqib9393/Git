c8c3dd5530dcb2d9abe01b0a28cb410ecf79c352
aqib9393 <63776916+aqib9393@users.noreply.github.com>
2022-11-25 14:55:25 +0500

    .

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask , request, Response
 import json
 import base64
 from werkzeug.utils import secure_filename
-#from flask_cors import CORS
+from flask_cors import CORS
 import cv2
 import imutils
 
